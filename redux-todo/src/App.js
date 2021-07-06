f11e5bdad3f1eecfb3de0bbdb5cc90bac0782b97
Jungwon Lee <leejungwon96@gmail.com>
2021-07-07 08:02:00 +0900

    feat: inputForm Add로직 구현 완료

--- redux-todo/src/App.js
+++ redux-todo/src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./commons/store";
 import * as S from "./App.styles";
 import InputForm from "./components/InputForm";
 import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <S.Container>
-      <S.Wrapper>
-        <h1>Redux로 배우는 Todo List</h1>
-        <InputForm />
-        <TodoList />
-      </S.Wrapper>
-    </S.Container>
+    <Provider store={store}>
+      <S.Container>
+        <S.Wrapper>
+          <h1>Redux로 배우는 Todo List</h1>
+          <InputForm />
+          <TodoList />
+        </S.Wrapper>
+      </S.Container>
+    </Provider>
   );
 }
 
--- redux-todo/src/components/InputForm.js
+++ redux-todo/src/components/InputForm.js
@@ -7,6 +7,33 @@ import { add_todo } from "../commons/actions";
 const InputForm = () => {
   const dispatch = useDispatch();
   const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    const todo = { title: text, isComplete: false };
+
+    dispatch(add_todo(todo));
+    setText("");
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") handleClick();
+  };
+
+  return (
+    <S.Container>
+      <S.InputBox
+        placeholder="할 일을 입력하세요"
+        value={text}
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+      ></S.InputBox>
+      <S.Button>추가하기</S.Button>
+    </S.Container>
+  );
 };
 
 export default InputForm;
